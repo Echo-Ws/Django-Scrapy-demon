be2c159b2d09b12d463d0674e40668bab19b882a
ws <695099544@qq.com>
2016-08-09 23:43:00 +0800

    improve url map优化网址映射

--- lecture/urls.py
+++ lecture/urls.py
@@ -4,9 +4,6 @@ from . import views
 
 urlpatterns = [
     # url(r'^$', views.IndexView.as_view(), name='index'),
-    url(r'^$', views.upTime, name='index'),
-    url(r'upT/$', views.upTime, name='upTime'),
-    url(r'downT/$', views.downTime, name='downTime'),
-    url(r'upU/$', views.upU, name='upU'),
-    url(r'downU/$', views.downU, name='downU'),
+    url(r'^$', views.index, name='index'),
+
     ]
--- lecture/views.py
+++ lecture/views.py
@@ -7,7 +7,18 @@ from .models import Lecture
 # Create your views here.
 
 
-def page(contact_list, request):
+def index(request):
+
+    o = request.GET.get('o')
+    if not o:
+        o = 'time'
+
+    if o[0].isalpha():
+        a = '-'
+    else:
+        a = ''
+
+    contact_list = Lecture.objects.order_by(o)
     paginator = Paginator(contact_list, 5)  # Show 15 contacts per page
     page = request.GET.get('page')
     try:
@@ -19,36 +30,10 @@ def page(contact_list, request):
     except EmptyPage:
         # If page is out of range (e.g. 9999), deliver last page of results.
         contacts = paginator.page(paginator.num_pages)
-    return contacts
-
-
-def upTime(request):
-    page = request.GET.get('page')
-
-
 
     return render(request, 'lecture/index.html', {
-        'lecture_list': page(Lecture.objects.order_by("time"), request),
-        'messageT': "downT/",
+        'lecture_list': contacts,
+        'message': a,
     })
 
 
-def downTime(request):
-    return render(request, 'lecture/index.html', {
-        'lecture_list': page(Lecture.objects.order_by("-time"), request),
-        'messageT': "upT/",
-    })
-
-
-def upU(request):
-    return render(request, 'lecture/index.html', {
-        'lecture_list': page(Lecture.objects.order_by("update_time"), request),
-        'messageU': "downU/",
-    })
-
-
-def downU(request):
-    return render(request, 'lecture/index.html', {
-        'lecture_list': page(Lecture.objects.order_by("-update_time"), request),
-        'messageU': "upU/",
-    })
